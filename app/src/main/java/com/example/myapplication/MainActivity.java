a6504222a7c5bbe12a87ebc53cfb73587a651b6d
Lastro21 <ptokarev8@gmail.com>
2021-01-17 12:25:56 +0300

    Added okhttp3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,8 +7,17 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -17,6 +26,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -39,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 List<Post> posts = response.body();
 
+
                 for (Post post : posts) {
                     System.out.println(post.getBody());
                     System.out.println(post.getId());
@@ -53,15 +65,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-
     }
 
     public void onButtonClick22(final View view) {
-
-
-
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
@@ -93,12 +100,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
-
         final EditText el1 = findViewById(R.id.Num1);
         final EditText el2 = findViewById(R.id.Num2);
         final TextView resText = findViewById(R.id.Result);
@@ -111,4 +112,84 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void onButtonClickGET(final View view) throws IOException {
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                Request request = new Request.Builder()
+                        .url("https://reqres.in/api/users?page=2")
+                        .build();
+                OkHttpClient client = new OkHttpClient();
+                try {
+                    okhttp3.Response response = client.newCall(request).execute();
+                    System.out.println(response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        System.out.println("GGG");
+
+    }
+
+    public void onButtonClickPOST(final View view) throws IOException {
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+                RequestBody body = RequestBody.create(JSON, "{\"jsonExample\":\"value\"}");
+
+                Request request = new Request.Builder()
+                        .url("https://reqres.in/api/users?page=2")
+                        .post(body)
+                        .build();
+                OkHttpClient client = new OkHttpClient();
+                try {
+                    okhttp3.Response response = client.newCall(request).execute();
+                    System.out.println(response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        System.out.println("GGG");
+
+    }
+
+    public void onButtonClickPostSync(final View view) throws IOException {
+
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(JSON, "{\"jsonExample\":\"value\"}");
+
+        Request request = new Request.Builder()
+                .url("https://reqres.in/api/users?page=2")
+                .post(body)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+
+        okhttp3.Call call = client.newCall(request);
+
+        call.enqueue(new okhttp3.Callback() {
+
+            @Override
+            public void onResponse(@NotNull okhttp3.Call call, @NotNull okhttp3.Response response) throws IOException {
+                System.out.println(response.body().string());
+            }
+
+            @Override
+            public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println("GGG");
+
+    }
 }
